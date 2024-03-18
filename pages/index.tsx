09495d3cf5c4aec0048997ea3cbb181d6ab6735c
nekokusa420 <sk011010ab@gmail.com>
2024-03-19 03:12:15 +0800

    98

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }) => {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
       {/* yarn add -D tailwind-scrollbar */}
       <Head>
-        <title>Kit's portfolio</title>
+        <title>Meow portfolio</title>
       </Head>
       {/* <h1 className="text-red-800">Lets build and AWESOME portfolio</h1> */}
       {/* Header */}
